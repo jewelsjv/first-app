dc4447fdfb56f85daba5d7a35ac626502f2c5984
jewelsjv <juvern.see@gmail.com>
2014-04-20 20:35:55 +0100

    Add seller page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Etsydemo::Application.routes.draw do
   get "pages/about"
   get "pages/contact"
 
+  #adding a page that relates to listing
+  get 'seller' => 'listings#seller'
+
   root 'listings#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
