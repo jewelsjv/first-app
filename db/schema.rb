fb49ccb7bf69f93257488c61242ee892663de444
jewelsjv <juvern.see@gmail.com>
2014-05-11 18:05:52 +0100

    Transfer funds to seller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140420205841) do
+ActiveRecord::Schema.define(version: 20140511161417) do
 
   create_table "listings", force: true do |t|
     t.string   "name"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20140420205841) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+    t.string   "recipient"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
